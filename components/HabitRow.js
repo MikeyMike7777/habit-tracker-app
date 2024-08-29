f62f9321256d6ea8df36d7cf362bacb7690d1e24
MikeyMike7777 <michaelm246810@gmail.com>
2024-08-29 16:09:06 -0500

    Styling project structure fixes

--- components/HabitRow.js
+++ components/HabitRow.js
@@ -1,84 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
-import { Fonts } from '../constants/Fonts';
+import { HabitRowStyles } from './styles/HabitRowStyles';
 
 export default function HabitRow({ title, count1, count2, iconColor1 }) {
   return (
-    <View style={styles.rowContainer}>
-      <View style={styles.leftSection}>
-        <Text style={styles.title}>{title}</Text>
+    <View style={HabitRowStyles.rowContainer}>
+      <View style={HabitRowStyles.leftSection}>
+        <Text style={HabitRowStyles.title}>{title}</Text>
       </View>
-      <View style={styles.middleLeftSection}>
-        <View style={styles.iconRow}>
+      <View style={HabitRowStyles.middleLeftSection}>
+        <View style={HabitRowStyles.iconRow}>
           <Ionicons name="checkmark-circle" size={20} color={iconColor1} />
-          <Text style={styles.count}>{count1}</Text>
+          <Text style={HabitRowStyles.count}>{count1}</Text>
         </View>
     </View>
-    <View style={styles.middleRightSection}>
-        <View style={styles.iconRow}>
+    <View style={HabitRowStyles.middleRightSection}>
+        <View style={HabitRowStyles.iconRow}>
           <Ionicons name="checkmark-circle" size={20} color={Colors.gold} />
-          <Text style={styles.count}>{count2}</Text>
+          <Text style={HabitRowStyles.count}>{count2}</Text>
         </View>
       </View>
-      <View style={styles.rightSection}>
+      <View style={HabitRowStyles.rightSection}>
         <Ionicons name="information-circle-outline" size={25} color={Colors.black} />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  rowContainer: {
-    width: "100%",
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey',
-  },
-  leftSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flex: 1,
-    paddingLeft: 20,
-  },
-  middleLeftSection: {
-    flexDirection: 'row',
-    alignItems: 'left',
-    flex: 1,
-    justifyContent: 'left',
-  },
-  middleRightSection: {
-    flexDirection: 'row',
-    alignItems: 'left',
-    flex: 1,
-    justifyContent: 'left',
-  },
-  rightSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'flex-end',
-    paddingRight: 20,
-  },
-  title: {
-    fontFamily: Fonts.AvenirHeavy,
-    fontSize: 18,
-    color: Colors.black,
-    marginRight: 10,
-  },
-  iconRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 10,
-    paddingHorizontal: 20,
-  },
-  count: {
-    marginLeft: 5,
-    fontSize: 16,
-    color: Colors.black,
-  },
-});
--- constants/Fonts.js
+++ constants/Fonts.js
@@ -1,6 +1,7 @@
 import * as Font from 'expo-font';
 
 export const Fonts = {
+  AvenirMedium: 'Avenir-Medium',
   AvenirBlack: 'Avenir-Black',
   AvenirBlackOblique: 'Avenir-BlackOblique',
   AvenirHeavy: 'Avenir-Heavy',
@@ -10,9 +11,11 @@ export const Fonts = {
 };
 
 Font.loadAsync({
+  [Fonts.AvenirMedium]: require('../assets/fonts/avenir/AvenirLTProMedium.otf'),
   [Fonts.AvenirBlack]: require('../assets/fonts/avenir/AvenirLTProBlack.otf'),
   [Fonts.AvenirBlackOblique]: require('../assets/fonts/avenir/AvenirLTProBlackOblique.otf'),
   [Fonts.AvenirHeavy]: require('../assets/fonts/avenir/AvenirLTProHeavy.otf'),
+  [Fonts.AvenirHeavyOblique]: require('../assets/fonts/avenir/AvenirLTProHeavyOblique.otf'),
   [Fonts.AvenirHeavyOblique]: require('../assets/fonts/avenir/AvenirLTProHeavyOblique.otf'),
   [Fonts.Copperplate]: require('../assets/fonts/Copperplate.otf'),
   [Fonts.Baskerville]: require('../assets/fonts/LibreBaskerville-Italic.ttf'),
